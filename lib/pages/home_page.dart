405d0badc37ff3c27d807592cf5d738348fa9c1c
Aiperi Ormonbekova <aiperi.ormonbekovna@gmail.com>
2023-04-17 10:53:38 +0600

    register_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+  final user = FirebaseAuth.instance.currentUser!;
+
+  void signUserOut() {
+    FirebaseAuth.instance.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('LOGED IN'),),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        actions: [
+          IconButton(
+            onPressed: signUserOut,
+            icon: const Icon(
+              Icons.logout,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          "LOGED IN AS:" + user.email!,
+          style: const TextStyle(fontSize: 20),
+        ),
+      ),
     );
   }
 }
